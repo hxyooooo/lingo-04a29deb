13f8cc355f6d45c3cfa0fee1e75cbdf3a1d233ec
lingo-auto-commit-robot <lingo-auto-commit-robot@alibabacloud.com>
2026-01-06 08:29:42 +0000

    更新Vite配置文件

---
+++ vite.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    // 允许内网穿透访问
+    allowedHosts: true,
+    cors: true
+  }
+})
