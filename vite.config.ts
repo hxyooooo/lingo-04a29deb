e9a41235e1e6905479e039677c3a1b382bf9d1d9
lingo-auto-commit-robot <lingo-auto-commit-robot@alibabacloud.com>
2026-01-06 08:10:59 +0000

    user manually modify file

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    host: true, // 监听所有地址
-    
-    // 【关键修改】添加 allowedHosts 允许该域名访问
-    allowedHosts: [
-      'lingo.console.aliyun.com'
-    ],
-    
-    // 如果您不想一个个添加域名，可以使用 'all' 允许所有主机访问（方便开发，但生产环境慎用）
-    // allowedHosts: 'all', 
+    host: true,
+    allowedHosts: true
   },
-
   define: {
     global: 'globalThis',
   },
